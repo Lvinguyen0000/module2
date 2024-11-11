de5d050361013527db74379884d23efad27c84c2
Lâm Vĩ Nguyên <80013922+Lvinguyen0000@users.noreply.github.com>
2024-11-11 14:13:27 +0700

    Delete src directory

--- src/Time.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class Time {
-    public static void main(String[] args) {
-        Date time = new Date();
-        System.out.println("Now is: " + time);
-    }
-}
